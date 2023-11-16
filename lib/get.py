93944dd0e1ecfca5ef0974fef5d3ba088164d256
Aftermath001 <alvinasiachi@gmail.com>
2023-11-16 14:53:01 +0300

    Get Requests

--- lib/get.py
+++ lib/get.py
@@ -1,10 +1,13 @@
 import requests
 import json
+# HTTP GET REQUESTS
 
+# url = "https://learn-co-curriculum.github.io/json-site-example/"
+# response = requests.get(url)
+# print(response.text)
+
+# Dealing with requets in JSON
 url = "https://learn-co-curriculum.github.io/json-site-example/endpoints/locations.json"
-
 response = requests.get(url)
-
 json_content = json.loads(response.text)
-
-print(json.dumps(json_content, indent=4))
+print(json.dumps(json_content, indent=4, sort_keys=True))
